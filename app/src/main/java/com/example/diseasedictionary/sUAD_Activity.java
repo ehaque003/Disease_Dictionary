680167fbb39382fdd23b393ed38a6e167704f9b4
ehaque003 <haqskids@gmail.com>
2022-05-21 13:30:51 -0700

    added sUAD_Activity

--- app/src/main/java/com/example/diseasedictionary/sUAD_Activity.java
+++ app/src/main/java/com/example/diseasedictionary/sUAD_Activity.java
@@ -10,5 +10,6 @@ public class sUAD_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suad);
+
     }
 }
