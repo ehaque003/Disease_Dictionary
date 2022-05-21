d77de42c0d4957f29bf3c4dbf0ed3b2e5b1b39cd
ehaque003 <haqskids@gmail.com>
2022-05-21 13:13:13 -0700

    Initial commit

---
+++ app/src/main/java/com/example/diseasedictionary/sUAD_Activity.java
@@ -0,0 +1,14 @@
+package com.example.diseasedictionary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sUAD_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suad);
+    }
+}
