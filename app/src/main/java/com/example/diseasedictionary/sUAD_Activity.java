a8712925781e5c76ab16db7485ef7f2830934ee8
blahblah23406 <fasT1432>
2022-05-22 02:46:39 -0700

    added database method

--- app/src/main/java/com/example/diseasedictionary/sUAD_Activity.java
+++ app/src/main/java/com/example/diseasedictionary/sUAD_Activity.java
@@ -4,6 +4,7 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
@@ -45,28 +46,46 @@ public class sUAD_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 String diseaseEntry = searchBox.getText().toString();
-                try {
-                    handler.jsonFormatter(diseaseEntry);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                SQLiteDatabase db = dbHelper.getReadableDatabase();
+                String temp = "1"+diseaseEntry+"1";
+                temp = temp.replace('1', (char)34);
+                String query = "SELECT "+AppDataRepo.SearchEntry.COLUMN_NAME_DIAGNOSIS+", "+AppDataRepo.SearchEntry.COLUMN_NAME_TRANSMISSIONS+", "+AppDataRepo.SearchEntry.COLUMN_NAME_TREATMENT+", "+AppDataRepo.SearchEntry.COLUMN_NAME_PREVENTION+", "+AppDataRepo.SearchEntry.COLUMN_NAME_FACTS+", "+AppDataRepo.SearchEntry.COLUMN_NAME_SYMPTOMS+" FROM " + AppDataRepo.SearchEntry.TABLE_NAME + " WHERE " + AppDataRepo.SearchEntry.COLUMN_NAME_DISEASENAME + " = " + temp;
+                Cursor c = db.rawQuery(query, null);
+                boolean result = c.getCount() > 0;
+                if(result){
+                    c.moveToFirst();
+                    infos[0] = c.getString(5);
+                    infos[1] = c.getString(1);
+                    infos[2] = c.getString(2);
+                    infos[3] = c.getString(4);
+                    infos[4] = c.getString(0);
+                    infos[5] = c.getString(3);
+                    c.close();
+                    db.close();
                 }
-                if(handler.response23.isEmpty()){
-                    Log.d("Warning", "Empty");
+                else{
+                    try {
+                        handler.jsonFormatter(diseaseEntry);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    if(handler.response23.isEmpty()){
+                        Log.d("Warning", "Empty");
+                    }
+                    try{
+                        JSONObject jsonObject = new JSONObject(handler.response23);
+                        infos[0] = jsonObject.getJSONObject("disease").getString("symptoms");
+                        infos[1] = jsonObject.getJSONObject("disease").getString("transmission");
+                        infos[2] = jsonObject.getJSONObject("disease").getString("treatment");
+                        infos[4] = jsonObject.getJSONObject("disease").getString("diagnosis");
+                        infos[5] = jsonObject.getJSONObject("disease").getString("prevention");
+                        infos[3] = jsonObject.getJSONObject("disease").getJSONArray("facts").getString(1);
+                    } catch (JSONException jsonException){
+                        jsonException.printStackTrace();
+                        Log.d("Warning", "You dead");
+                    }
+                    writeToDB(diseaseEntry);
                 }
-                try{
-                    JSONObject jsonObject = new JSONObject(handler.response23);
-                    infos[0] = jsonObject.getJSONObject("disease").getString("symptoms");
-                    infos[1] = jsonObject.getJSONObject("disease").getString("transmission");
-                    infos[2] = jsonObject.getJSONObject("disease").getString("treatment");
-                    infos[4] = jsonObject.getJSONObject("disease").getString("diagnosis");
-                    infos[5] = jsonObject.getJSONObject("disease").getString("prevention");
-                    infos[3] = jsonObject.getJSONObject("disease").getJSONArray("facts").getString(1);
-                } catch (JSONException jsonException){
-                    jsonException.printStackTrace();
-                    Log.d("Warning", "You dead");
-                }
-                writeToDB(diseaseEntry);
-
                 searchBox.setVisibility(View.INVISIBLE);
                 search.setVisibility(View.INVISIBLE);
                 info.setVisibility(View.VISIBLE);
